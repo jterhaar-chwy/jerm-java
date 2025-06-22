339d42d3d3163cdfb0fe2c888c4d741abcd17597
jeremy <jterhaar@chewy.com>
2025-06-21 21:54:26 -0500

    initial commit of spring boot app

---
+++ src/main/java/jerm/jerm_java/JermJavaApplication.java
@@ -0,0 +1,13 @@
+package jerm.jerm_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JermJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JermJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jerm/jerm_java/JermJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jerm.jerm_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JermJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
